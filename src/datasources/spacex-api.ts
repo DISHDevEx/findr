1574fdd1077bd3f6739ce35c93cd49119e7371ab
praveen.mada <praveen.mada@dish.com>
2023-07-14 14:24:39 -0600

    index.ts updates

--- src/datasources/spacex-api.ts
+++ src/datasources/spacex-api.ts
@@ -2,7 +2,7 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 
  export class SpacexAPI extends RESTDataSource {
 
-  baseURL = "https://api.spacexdata.com/v4/";
+  baseURL = "https://api.spacexdata.com/v3/";
 
   getMissions() {
     return this.get("missions");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+//import { ApolloServerPluginInlineTrace } from "apollo-server-core";
 import { typeDefs } from "./schemas/spacex.js";
 import { resolvers } from "./resolvers/spacex.js";
 import { SpacexAPI } from "./datasources/spacex-api.js";
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs, resolvers })
+  const server = new ApolloServer({ typeDefs, resolvers,
+    //plugins: [ApolloServerPluginInlineTrace()], 
+  })
   const { url } = await startStandaloneServer(server, {
     context: async () => {
       const { cache } = server;
