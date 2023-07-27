2d76066b2f002628d465311f9b7086c846fb56d9
praveen.mada <praveen.mada@dish.com>
2023-07-27 15:25:56 -0600

    updated app to run as https instead of http

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import https from 'https';
 import express from 'express';
+import fs from 'fs';
 import { ApolloServer, gql } from 'apollo-server-express';
 import { typeDefs } from "./schemas/spacex.js";
 import { resolvers } from "./resolvers/spacex.js";
 import { SpaceXAPI } from "./datasources/spacex-api.js";
+
+// Import the self-signed certificate files
+const privateKey = fs.readFileSync('openssl/key.pem', 'utf8');
+const certificate = fs.readFileSync('openssl/cert.pem', 'utf8');
 
 const server = new ApolloServer({
   typeDefs,
@@ -16,12 +22,16 @@ async function startApolloServer() {
   await server.start();
 
   const app = express();
+  
+  // Create the HTTPS server with self-signed certificates
+  const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
+
   server.applyMiddleware({ app });
 
-  const PORT = 4000;
-  app.listen({ port: PORT }, () =>
-    console.log(`Apollo Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-  );
+  const PORT = 3000;
+  httpsServer.listen(PORT, () => {
+    console.log(`Apollo Server ready at https://localhost:${PORT}${server.graphqlPath}`);
+  });
 }
 
 startApolloServer().catch((err) => console.error('Error starting Apollo server:', err));
