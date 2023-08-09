6f9d3e1fea8066f92121837110a6031e98ebc4dd
praveen.mada <praveen.mada@dish.com>
2023-08-09 14:11:50 -0600

    added voyager to visualize federated graphql schema

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,29 +1,38 @@
 import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware/index.js';
+import { printSchema } from 'graphql';
 
+// Create an Apollo Gateway instance
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
-      { name: "spacex", url: "http://localhost:4001/graphql" }
+      { name: 'spacex', url: 'http://localhost:4001/graphql' }
     ]
   })
 });
 
-// Create ApolloServer with the loaded schema and executor
+// Create an Apollo Server instance
 const server = new ApolloServer({
   gateway
 });
 
+// Start the Apollo Gateway server
 async function startApolloGateway() {
   await server.start();
 
   const app = express();
+
+  // Apply the Apollo Server middleware
   server.applyMiddleware({ app });
+
+  // Add GraphQL Voyager route
+  app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 
   const PORT = 4000;
   app.listen({ port: PORT }, () =>
-    console.log(`Apollo Gateway ready at http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(`Apollo Gateway and Voyager ready at http://localhost:${PORT}`)
   );
 }
 
