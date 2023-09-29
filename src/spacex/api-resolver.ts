9c14fb490d138a031841789a1a15ed0085eedf83
praveen.mada <praveen.mada@dish.com>
2023-09-29 16:19:14 -0600

    converting resolvers to class and renaming connectors to adapters

--- src/spacex/api-resolver.ts
+++ src/spacex/api-resolver.ts
@@ -1,79 +1,76 @@
 /**
- * This file contains the SpaceXResolvers object which defines the resolvers for the SpaceX API.
- * The Launch resolver fetches the rocket information for a given launch.
- * The Query resolver provides functions to fetch capsules, rockets, and launches from the SpaceX API.
- * @packageDocumentation
+ * This class defines the resolvers for the SpaceX API.
+ * The `getRocketById` resolver fetches the rocket information for a given launch.
+ * The `getCapsules` resolver provides a function to fetch capsules from the SpaceX API.
+ * The `getRockets` resolver provides a function to fetch rockets from the SpaceX API.
+ * The `getLaunches` resolver provides a function to fetch launches from the SpaceX API, along with their associated capsules and rockets.
  */
-
 /**
  * This file contains the SpaceXResolvers object which defines the resolvers for the SpaceX API.
  * The Launch resolver fetches the rocket information for a given launch.
  * The Query resolver provides functions to fetch capsules, rockets, and launches from the SpaceX API.
  * @packageDocumentation
  */
-export const SpaceXResolvers = {
-  Launch: {
-    rocket: async (launch: any, _args: any, { dataSources }: any) => {
-      try {
-        const rocketId = launch.rocket.id;
-        //console.log("Fetching rocket with ID:", rocketId);
-        const rocket = await dataSources.spaceXAPI.getRocketById(rocketId);
-        //console.log("Fetched rocket:", rocket);
-        return rocket;
-      } catch (error) {
-        // Handle the "404: Not Found" error by returning null or an appropriate response
-        console.error("Error fetching rocket:", error.message);
-        return null;
-      }
-    },
-  },
 
-  Query: {
-    spacexCapsules: async (_source: any, _args: any, { dataSources }: any) => {
-      return dataSources.spaceXAPI.getCapsules();
-    },
+export class SpaceXResolvers {
+  async getRocketById(launch: any, _args: any, { dataSources }: any) {
+    try {
+      const rocketId = launch.rocket.id;
+      //console.log("Fetching rocket with ID:", rocketId);
+      const rocket = await dataSources.spaceXAPI.getRocketById(rocketId);
+      //console.log("Fetched rocket:", rocket);
+      return rocket;
+    } catch (error) {
+      // Handle the "404: Not Found" error by returning null or an appropriate response
+      console.error("Error fetching rocket:", error.message);
+      return null;
+    }
+  }
 
-    spacexRockets: async (_source: any, _args: any, { dataSources }: any) => {
-      try {
-        const rockets = await dataSources.spaceXAPI.getRockets();
-        return rockets;
-      } catch (error) {
-        // Handle the "404: Not Found" error by returning an empty array or an appropriate response
-        console.error("Error fetching rockets:", error.message);
-        return [];
-      }
-    },
+  async getCapsules(_source: any, _args: any, { dataSources }: any) {
+    return dataSources.spaceXAPI.getCapsules();
+  }
 
-    spacexLaunches: async (_source: any, _args: any, { dataSources }: any) => {
-      try {
-        const launches = await dataSources.spaceXAPI.getLaunches();
+  async getRockets(_source: any, _args: any, { dataSources }: any) {
+    try {
+      const rockets = await dataSources.spaceXAPI.getRockets();
+      return rockets;
+    } catch (error) {
+      // Handle the "404: Not Found" error by returning an empty array or an appropriate response
+      console.error("Error fetching rockets:", error.message);
+      return [];
+    }
+  }
 
-        const launchesWithCapsulesAndRockets = await Promise.all(
-          launches.map(async (launch: any) => {
-            const capsules = await Promise.all(
-              launch.capsules.map(async (capsuleId: string) => {
-                return dataSources.spaceXAPI.getCapsuleById(capsuleId);
-              })
-            );
+  async getLaunches(_source: any, _args: any, { dataSources }: any) {
+    try {
+      const launches = await dataSources.spaceXAPI.getLaunches();
 
-            try {
-              const rocketId = launch.rocket;
-              const rocket = await dataSources.spaceXAPI.getRocketById(rocketId);
+      const launchesWithCapsulesAndRockets = await Promise.all(
+        launches.map(async (launch: any) => {
+          const capsules = await Promise.all(
+            launch.capsules.map(async (capsuleId: string) => {
+              return dataSources.spaceXAPI.getCapsuleById(capsuleId);
+            })
+          );
 
-              return { ...launch, capsules, rocket };
-            } catch (error) {
-              console.error("Error fetching rocket for launch:", error.message);
-              return { ...launch, capsules };
-            }
-          })
-        );
+          try {
+            const rocketId = launch.rocket;
+            const rocket = await dataSources.spaceXAPI.getRocketById(rocketId);
 
-        return launchesWithCapsulesAndRockets;
-      } catch (error) {
-        // Handle other errors that may occur during launch data retrieval
-        console.error("Error fetching launches:", error.message);
-        return [];
-      }
-    },
-  },
-};
+            return { ...launch, capsules, rocket };
+          } catch (error) {
+            console.error("Error fetching rocket for launch:", error.message);
+            return { ...launch, capsules };
+          }
+        })
+      );
+
+      return launchesWithCapsulesAndRockets;
+    } catch (error) {
+      // Handle other errors that may occur during launch data retrieval
+      console.error("Error fetching launches:", error.message);
+      return [];
+    }
+  }
+}
