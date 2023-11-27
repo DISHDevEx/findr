1e756d87496642d4cfcf83391c3c85aaac5b4263
John Liu <zheng.liu@dish.com>
2023-11-27 12:06:09 -0700

    update tsconfig.json

--- src/adapters/s3.ts
+++ src/adapters/s3.ts
@@ -1,5 +1,5 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { Readable } from 'stream';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { type Readable } from 'stream'
 
 /**
  * S3Uploader is a class for uploading objects to an Amazon S3 bucket.
@@ -10,21 +10,21 @@ export class S3Uploader {
    * @type {S3Client}
    * @private
    */
-  private readonly s3: S3Client;
+  private readonly s3: S3Client
 
   /**
    * Name of the Amazon S3 bucket.
    * @type {string}
    * @private
    */
-  private readonly bucketName: string;
+  private readonly bucketName: string
 
   /**
    * Key to identify the uploaded object within the bucket.
    * @type {string}
    * @private
    */
-  private readonly fileKey: string;
+  private readonly fileKey: string
 
   /**
    * Constructs an S3Uploader instance.
@@ -33,10 +33,10 @@ export class S3Uploader {
    * @param {string} fileKey - The key to identify the uploaded object within the bucket.
    * @param {string} region - The AWS region where the S3 bucket is located.
    */
-  constructor(bucketName: string, fileKey: string, region: string) {
-    this.s3 = new S3Client({ region });
-    this.bucketName = bucketName;
-    this.fileKey = fileKey;
+  constructor (bucketName: string, fileKey: string, region: string) {
+    this.s3 = new S3Client({ region })
+    this.bucketName = bucketName
+    this.fileKey = fileKey
   }
 
   /**
@@ -45,7 +45,7 @@ export class S3Uploader {
    * @param {Buffer | Readable} data - The data to be uploaded, either as a Buffer or Readable stream.
    * @returns {Promise<void>} A Promise that resolves when the upload is successful, or rejects on error.
    */
-  async uploadObject(data: Buffer | Readable): Promise<void> {
+  async uploadObject (data: Buffer | Readable): Promise<void> {
     /**
      * Parameters for the S3 PutObject command.
      * @type {Object}
@@ -54,17 +54,17 @@ export class S3Uploader {
     const params = {
       Bucket: this.bucketName,
       Key: this.fileKey,
-      Body: data,
-    };
+      Body: data
+    }
 
-    const command = new PutObjectCommand(params);
+    const command = new PutObjectCommand(params)
 
     try {
-      await this.s3.send(command);
+      await this.s3.send(command)
       // Uncomment the following line for logging: console.log(`Object uploaded to S3: s3://${this.bucketName}/${this.fileKey}`);
     } catch (error) {
       // Uncomment the following line for logging: console.error(`Error uploading object to S3: ${error.message}`);
-      throw error;
+      throw error
     }
   }
 }
