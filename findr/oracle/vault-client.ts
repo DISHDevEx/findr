c03a4437508edd5724cfeceb1fddbea94973c2cd
John Liu <zheng.liu@dish.com>
2023-12-14 14:28:53 -0700

    update: vault-client.ts

--- findr/oracle/vault-client.ts
+++ findr/oracle/vault-client.ts
@@ -1,38 +1,91 @@
-import axios from 'axios';
+/**
+ * VaultClient Class
+ *
+ * The `VaultClient` class is a TypeScript implementation for interacting with HashiCorp Vault.
+ * It provides methods for authentication, reading secrets, and writing secrets.
+ */
+import axios, { type AxiosResponse } from 'axios'
 
+/**
+ * Class representing a client for interacting with HashiCorp Vault.
+ */
 class VaultClient {
-  private vaultUrl: string;
-  private token: string | null = null;
+  /**
+   * The base URL of the HashiCorp Vault instance.
+   * @private
+   */
+  private readonly vaultUrl: string
 
-  constructor(vaultUrl: string) {
-    this.vaultUrl = vaultUrl;
+  /**
+   * The authentication token for the Vault instance.
+   * @private
+   */
+  private token: string | null = null
+
+  /**
+   * Creates an instance of `VaultClient`.
+   * @param vaultUrl - The base URL of the HashiCorp Vault instance.
+   */
+  constructor (vaultUrl: string) {
+    this.vaultUrl = vaultUrl
   }
 
-  async authenticate(token: string): Promise<void> {
-    this.token = token;
+  /**
+   * Encodes special characters in a given input string.
+   * @private
+   * @param input - The input string to be encoded.
+   * @returns The encoded string.
+   */
+  private encodeSpecialCharacters (input: string): string {
+    return encodeURIComponent(input)
   }
 
-  async readSecret(path: string): Promise<any> {
-    if (!this.token) {
-      throw new Error('Client is not authenticated');
+  /**
+   * Authenticates the client with the provided token.
+   * @param token - The authentication token.
+   * @throws {Error} - If the client is not authenticated.
+   */
+  async authenticate (token: string): Promise<void> {
+    this.token = token
+  }
+
+  /**
+   * Reads secrets from a specified path.
+   * @param path - The path to the secret.
+   * @returns A Promise resolving to the secret data.
+   * @throws {Error} - If the client is not authenticated.
+   */
+  async readSecret (path: string): Promise<any> {
+    if (this.token === null || this.token === undefined) {
+      throw new Error('Client is not authenticated')
     }
 
-    const response = await axios.get(`${this.vaultUrl}/v1/${path}`, {
-      headers: { 'X-Vault-Token': this.token }
-    });
+    const encodedPath = this.encodeSpecialCharacters(path)
 
-    return response.data.data;
+    const response: AxiosResponse = await axios.get(`${this.vaultUrl}/v1/cubbyhole/${encodedPath}`, {
+      headers: { 'X-Vault-Token': this.token, accept: '*/*' }
+    })
+
+    return response.data.data
   }
 
-  async writeSecret(path: string, secretData: any): Promise<void> {
-    if (!this.token) {
-      throw new Error('Client is not authenticated');
+  /**
+   * Writes secrets to a specified path.
+   * @param path - The path to write the secret.
+   * @param secretData - The data to be written as the secret.
+   * @throws {Error} - If the client is not authenticated.
+   */
+  async writeSecret (path: string, secretData: any): Promise<void> {
+    if (this.token === null || this.token === undefined) {
+      throw new Error('Client is not authenticated')
     }
 
-    await axios.post(`${this.vaultUrl}/v1/${path}`, secretData, {
-      headers: { 'X-Vault-Token': this.token }
-    });
+    const encodedPath = this.encodeSpecialCharacters(path)
+
+    await axios.post(`${this.vaultUrl}/v1/cubbyhole/${encodedPath}`, secretData, {
+      headers: { 'X-Vault-Token': this.token, 'Content-Type': 'application/json', accept: '*/*' }
+    })
   }
 }
 
-export default VaultClient;
+export default VaultClient
